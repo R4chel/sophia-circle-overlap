2e5a438eeadfc6682315dbc9920b425e296261d9
Rachel <rae84@cornell.edu>
2024-11-22 13:29:24 -0500

    start

---
+++ sketch_sophia_circle_overlap.py
@@ -0,0 +1,37 @@
+import vsketch
+from shapely.geometry import Point
+
+class SophiaCircleOverlapSketch(vsketch.SketchClass):
+    # Sketch parameters:
+    debug = vsketch.Param(False)
+    width = vsketch.Param(5., decimals=2, unit="in")
+    height = vsketch.Param(3., decimals=2, unit="in")
+    margin = vsketch.Param(0.1, decimals=3, unit="in")
+    landscape = vsketch.Param(False)
+    pen_width = vsketch.Param(0.7, decimals=3, min_value=1e-10, unit="mm")
+    num_layers = vsketch.Param(1)
+    # radius = vsketch.Param(1.0, decimals=3, unit="in")
+
+    def random_point(self, vsk: vsketch.Vsketch):
+        return Point(vsk.random(0, self.width), vsk.random(0, self.height))
+
+    def draw(self, vsk: vsketch.Vsketch) -> None:
+        vsk.size(f"{self.height}x{self.width}", landscape=self.landscape, center=False)
+        self.width = self.width - 2 * self.margin
+        self.height = self.height - 2 * self.margin
+        vsk.translate(self.margin, self.margin)
+        vsk.penWidth(f"{self.pen_width}")
+
+        # implement your sketch here
+        # layers = [1 + i for i in range(self.num_layers)]
+        # layer = layers[int(vsk.random(0, len(layers)))]
+        # vsk.stroke(layer)
+        # vsk.fill(layer)
+        # vsk.circle(0, 0, self.radius, mode="radius")
+
+    def finalize(self, vsk: vsketch.Vsketch) -> None:
+        vsk.vpype("linemerge linesimplify reloop linesort")
+
+
+if __name__ == "__main__":
+    SophiaCircleOverlapSketch.display()
